b197ba8e065295d64b01081ea26db4ed1b317be3
daltonhahn <hahnd11@gmail.com>
2021-07-13 13:50:27 -0500

    Making startup sequence

---
+++ scripts/starter.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+read -p "Client nodes: " numClients
+
+cd ..
+docker-compose start s1
+
+SList="s2 s3 s4 s5"
+
+for i in $(seq 1 $numClients)
+do
+	SList+=" c$i"
+
+done
+
+docker-compose start $SList
+
+cd scripts
